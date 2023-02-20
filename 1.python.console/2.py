6754e5e99a687b6a76580fe5ece9a97245521cf9
Aleksandr <avpoddubniy@yandex.ru>
2023-02-20 14:00:05 +0300

    Finish 01

---
+++ 1.python.console/2.py
@@ -0,0 +1,14 @@
+import math
+
+side = int(input('Введите длину стороны квадрата: '))
+
+
+print("Периметр:", side * 4)
+print('Площадь:', side * side)
+
+side_1 = int(input('Введите длину прямоугольника: '))
+side_2 = int(input('Введите ширину прямоугольника: '))
+
+print("Периметр:", 2 * (side_1 + side_2))
+print('Площадь:', side_1 * side_2)
+
